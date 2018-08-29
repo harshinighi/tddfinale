f110186bcf60c9fbeb207841aefb6d111e5b0cc5
Harshini <harshinighi@gmail.com>
2018-08-29 23:32:56 +0530

    sending....

--- com.tdd/src/test/java/Runner/Test_Runner.java
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -19,7 +19,7 @@ public void test()
 public void beforeTest()
 {
 	System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-	File pathBinary = new File("/usr/bin/firefox");
+	File pathBinary = new File("/usr/local/bin/firefox");
 	System.setProperty("webdriver.firefox.bin", pathBinary.getAbsolutePath());
 	//System.setProperty("webdriver.gecko.driver", "E:\\SHARED\\geckodriver\\geckodriver.exe");
 	//System.out.println("helllooo");
