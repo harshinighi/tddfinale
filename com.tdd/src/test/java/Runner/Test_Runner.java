6ab460f65051f2c0882cb1f809c2ecd68d899861
harshinighi <41231571+harshinighi@users.noreply.github.com>
2018-08-30 03:51:44 +0530

    Update Test_Runner.java

--- com.tdd/src/test/java/Runner/Test_Runner.java
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -4,29 +4,26 @@ import java.io.File;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
 import org.testng.annotations.AfterTest;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Test_Runner {
 WebDriver driver;
 @Test
 public void test() 
 {
-	System.out.println(driver.getTitle());
+	System.out.println("hi");
 	}
 @BeforeTest
 public void beforeTest()
 {
 	System.out.println("helllooo");
-	System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-	driver = new FirefoxDriver();
+	
 	//System.setProperty("webdriver.firefox.bin", pathBinary.getAbsolutePath());System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");
         //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	
-	driver.get("http://13.251.143.66:8090/IPL-Fantasy-League/");
+	//driver.get("http://13.251.143.66:8090/IPL-Fantasy-League/");
 
 }
 @AfterTest
