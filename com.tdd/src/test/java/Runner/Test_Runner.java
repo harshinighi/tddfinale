33fb39e248f2854a8822b01bab3af9ab40049941
Harshini <harshinighi@gmail.com>
2018-08-29 23:13:31 +0530

    uuuu

---
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -0,0 +1,34 @@
+package Runner;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test_Runner {
+WebDriver driver;
+@Test
+public void test() 
+{
+	System.out.println(driver.getTitle());
+	}
+@BeforeTest
+public void beforeTest()
+{
+	System.setProperty("webdriver.gecko.driver", "//usr//local//bin//geckodriver");
+	File pathBinary = new File("/usr/bin/firefox");
+	System.setProperty("webdriver.firefox.bin", pathBinary.getAbsolutePath());
+	//System.setProperty("webdriver.gecko.driver", "E:\\SHARED\\geckodriver\\geckodriver.exe");
+	//System.out.println("helllooo");
+	driver= new FirefoxDriver();
+	driver.get("http://13.251.143.66:8090/IPL-Fantasy-League/");
+
+}
+@AfterTest
+public void afterTest(){
+	System.out.println("Running1");
+}
+}
